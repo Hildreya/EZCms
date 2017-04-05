5cb093d107d24fc67c858138880388ca84123013
VectorFR <venard.paul@gmail.com>
2017-04-05 22:44:30 +0200

    Mise en place du listing des bundles

--- src/EZ/CoreBundle/Controller/DefaultController.php
+++ src/EZ/CoreBundle/Controller/DefaultController.php
@@ -3,11 +3,21 @@
 namespace EZ\CoreBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Yaml\Yaml;
 
 class DefaultController extends Controller
 {
-    public function indexAction()
+    public function BundleAction()
     {
-        return $this->render('EZCoreBundle:Default:index.html.twig');
+        $directories = __DIR__.'/../../';
+        $scanned_directory = array_diff(scandir($directories), array('..', '.'));
+
+        foreach ($scanned_directory as $directory) {
+
+            $bundles[$directory] = Yaml::parse(file_get_contents(__DIR__ . '/../../' . $directory . '/Resources/config/bundle.yml'));
+        }
+        return $this->render('EZCoreBundle:Default:module.html.twig', array(
+            'bundles' => $bundles
+        ));
     }
 }
