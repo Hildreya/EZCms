8b845b30f42e06b168ed41aafb89038778780682
Hildreya <hildreya@gmail.com>
2017-04-22 16:35:20 +0200

    Get article & comments

--- src/EZ/ArticleBundle/Controller/HomeController.php
+++ src/EZ/ArticleBundle/Controller/HomeController.php
@@ -21,10 +21,33 @@ class HomeController extends Controller
         ));
     }
 
+    public function selectAction($id) {
+        //Get the current template use
+        $template = Yaml::parse(file_get_contents(__DIR__ . '/../../CoreBundle/Resources/config/parameters.yml'));
+        $template = $template['parameters']['template'];
+
+        //Get Article
+        $article = $this->getArticleAction($id);
+        $comments = $article->getComment();
+
+        return $this->render('EZArticleBundle:office:select.html.twig', array(
+            'selectedTemplate' => "EZCoreBundle:Layout/office:". $template .".html.twig",
+            'article' => $article,
+            'comments' => $comments
+        ));
+    }
+
     public function getArticlesAction() {
         $em = $this->getDoctrine()->getManager();
         $articles = $em->getRepository('EZArticleBundle:Article')->findAll();
 
         return $articles;
+    }
+
+    public function getArticleAction($id) {
+        $em = $this->getDoctrine()->getManager();
+        $article = $em->getRepository('EZArticleBundle:Article')->find($id);
+
+        return $article;
     }
 }
