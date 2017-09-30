b1f9beb4fa1ff2406246dfb00550d67207acf232
Hildreya <hildreya@gmail.com>
2017-09-30 11:47:30 +0200

    /UserBundle: Uncommitted changes before Checkout at 30/09/2017 11:44

--- src/EZ/UserBundle/Form/RegistrationType.php
+++ src/EZ/UserBundle/Form/RegistrationType.php
@@ -5,12 +5,25 @@ namespace EZ\UserBundle\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 class RegistrationType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-
+        $builder
+            ->add('roles', ChoiceType::class, array(
+                'attr'  =>  array('class' => 'form-control select2',
+                    'style' => 'margin:5px 0;'),
+                'choices' =>
+                    array
+                    (
+                        'Administrateur' => 'ROLE_ADMIN',
+                        'Utilisateur' => 'ROLE_USER'
+                    ) ,
+                'multiple' => true,
+                'required' => true,
+            ));
     }
 
     public function configureOptions(OptionsResolver $resolver)
@@ -20,7 +33,7 @@ class RegistrationType extends AbstractType
 
     public function getParent()
     {
-        return 'fos_user_registration';
+        return 'FOS\UserBundle\Form\Type\RegistrationFormType';
     }
 
     public function getBlockPrefix()
