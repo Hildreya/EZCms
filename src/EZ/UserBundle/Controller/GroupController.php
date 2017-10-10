b5a041bb0a0ca54dcb4724ccd61d669dce5d7330
Hildreya <hildreya@gmail.com>
2017-10-10 23:12:03 +0200

    Config & fix module

---
+++ src/EZ/UserBundle/Controller/GroupController.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace EZ\UserBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class GroupController extends Controller
+{
+}
---
+++ src/EZ/UserBundle/Entity/Group.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: Matthieu
+ * Date: 09/10/2017
+ * Time: 23:25
+ */
+
+namespace EZ\UserBundle\Entity;
+
+
+class Group
+{
+
+}
